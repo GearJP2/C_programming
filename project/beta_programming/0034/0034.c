c3c76c51e0a99288b8c245725239a23b72a6877b
GearJP2 <rojsak.kritsuthikul@gmail.com>
2025-11-21 04:21:49 +0700

    update pc

--- project/beta_programming/0034/0034.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-// A = ac, B = ad+bc, C = bd
-
-int main()
-{
-    int A = 0, B = 0, C = 0;
-    
-}
--- project/beta_programming/025/0025_2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <string.h>
-
-
-int main() {
-    char skibidi = '1';
-    int digit = skibidi;
-    printf("%d", digit);
-}
