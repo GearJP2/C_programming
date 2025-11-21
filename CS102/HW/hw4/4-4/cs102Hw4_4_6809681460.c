a5073a62693e3e734f1d22fbe650a9636f966dd0
GearJP2 <rojsak.kritsuthikul@gmail.com>
2025-11-21 09:06:13 +0700

    hw4-4 update

--- CS102/HW/hw4/4-4/cs102Hw4_4_6809681460.c
+++ CS102/HW/hw4/4-4/cs102Hw4_4_6809681460.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
+#define size 100
 
-void findTax(struct myGoodRec a){
-
-}
-
-struct myGoodsRec 
+struct myGoodsRec
 {
     int ID;
     char name[20];
@@ -13,8 +10,76 @@ struct myGoodsRec
     float tax;
     float netPrice;
 };
+typedef struct myGoodsRec Goods;
+Goods myGoods[size];
 
-int main() {
-    typedef struct myGoodRec Goods;
-    Goods myGoods[100];
+void findTax(Goods myGoods[], int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        if (myGoods[i].type == 'A')
+        {
+            myGoods[i].tax = myGoods[i].cost * (0.1);
+        }
+        else if (myGoods[i].type == 'B')
+        {
+            myGoods[i].tax = myGoods[i].cost * (0.15);
+        }
+        else if (myGoods[i].type == 'C')
+        {
+            myGoods[i].tax = myGoods[i].cost * (0.2);
+        }
+        else if (myGoods[i].type == 'D')
+        {
+            myGoods[i].tax = myGoods[i].cost * (0.4);
+        }
+        else 
+        {
+            myGoods[i].tax = 0.0;
+        }
+        
+        myGoods[i].netPrice = myGoods[i].cost + myGoods[i].tax;
+        i++;
+    }
+}
+
+int main()
+{
+    int count = 0;
+    FILE *infile;
+    FILE *outfile;
+
+    if ((infile = fopen("Goods.txt", "r")) == NULL)
+        return printf("ERROR: FILE NOT FOUND\n");
+
+    while (count < size && 
+           fscanf(infile, "%d %s %s %f", 
+                   &myGoods[count].ID, myGoods[count].name, &myGoods[count].type, &myGoods[count].cost) == 4)
+    {
+        count++; 
+    }
+
+    findTax(myGoods, count); 
+    
+    fclose(infile);
+    
+    outfile = fopen("Report6809681460.txt", "w");
+    
+    if(outfile != NULL){
+        fprintf(outfile, "%-5s %-15s %-5s %10s %10s %12s\n", 
+                "ID", "Name", "Type", "Cost", "Tax", "NetPrice");
+
+        int i = 0;
+        while (i < count)
+        {
+            fprintf(outfile, "%-5d %-15s %-5c %10.2f %10.2f %12.2f\n", 
+                    myGoods[i].ID, myGoods[i].name, myGoods[i].type, 
+                    myGoods[i].cost, myGoods[i].tax, myGoods[i].netPrice); 
+            i++;
+        }
+        fclose(outfile);
+    }
+    
+    return 0;
 }
